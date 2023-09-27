7de77dd29983bbdf78adfc0167276e8fe6e56927
lisakozar <143966945+lisakozar@users.noreply.github.com>
2023-09-27 15:38:38 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,23 @@
 // JavaScript Document
 console.log("hi");
+
+var menuButton = document.querySelector("header > button");
+var hetMenu = document.querySelector("header nav");
+var closeButton = document.querySelector("header nav button");
+
+function toggleMenu() {
+	console.log("'t werkt");
+	hetMenu.classList.toggle("open");
+}
+
+menuButton.onclick = toggleMenu;
+closeButton.onclick = toggleMenu;
+
+
+
+
+
+
+
+
+
